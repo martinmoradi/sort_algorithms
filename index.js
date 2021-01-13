1e096a6fac43643dec8d596d775dee0e47626ac7
Martin Moradi <moradi.martin@gmail.com>
2021-01-13 10:59:02 +0100

    merge sort

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const fs = require("fs");
 const { type } = require("os");
 const { parse } = require("path");
+const { cpuUsage } = require("process");
 
 const data = process.argv[2];
 fs.readFile(data, "utf8", (error, data) => {
@@ -68,7 +69,7 @@ fs.readFile(data, "utf8", (error, data) => {
   const quickSort = (numbers) => {
     let counter = 0;
 
-    const sort = (numbers) => {    
+    const sort = (numbers) => {
       if (numbers.length <= 1) return numbers;
       const pivot = numbers[0];
       let right = [];
@@ -81,50 +82,42 @@ fs.readFile(data, "utf8", (error, data) => {
       return sort(left, counter).concat(pivot, sort(right, counter));
     };
 
-    numbers = sort(numbers)
+    numbers = sort(numbers);
     return `Tri rapide/QuickSort: ${counter} comparaisons [${numbers}]`;
+  };
+
+  // Tri fusion (merge sort)
+  const mergeSort = (arr_numbers) => {
+    let counter = 0;
+    let numbers = [...arr_numbers];
+    let half;
+
+    const merger = (left, right) => {
+      let arr = [];
+      while (left.length && right.length) {
+        if (left[0] < right[0]) {
+          arr.push(left.shift()); // remove from the left part and push into the sorted array
+        } else {
+          arr.push(right.shift()); // remove from the right part and push into the sorted array
+        }
+      }
+      return [...arr, ...left, ...right];
+    };
+
+    const mergeSortSplit = (numbers, half = numbers.length / 2) => {
+      counter++;
+      if (numbers.length < 2) return numbers;
+      let left = numbers.splice(0, half); //left part of numbers
+      return merger(mergeSortSplit(left), mergeSortSplit(numbers));
+    };
+
+    numbers = mergeSortSplit(arr_numbers, half);
+    return `Tri fusion/Merge Sort: ${counter} comparaisons [${numbers}]`;
   };
 
   console.log(bubbleSort(arr_numbers));
   console.log(insersionSort(arr_numbers));
   console.log(selectionSort(arr_numbers));
   console.log(quickSort(arr_numbers));
+  console.log(mergeSort(arr_numbers));
 });
-
-/* Je me suis beaucoup trop emmerdé : 
-let count = 0 
-  const partition = (numbers, leftIndex, rightIndex) => {
-    const pivot = numbers[rightIndex];
-    let i = leftIndex;
-    for (let j = leftIndex; j < rightIndex; j++) {
-      count++;
-      if (numbers[j] < pivot) {
-        [numbers[j], numbers[i]] = [numbers[i], numbers[j]];
-        i++;
-      }
-    }
-    [numbers[i], numbers[rightIndex]] = [numbers[rightIndex], numbers[i]];
-    return i;
-  };
-
-  const quickSort = (numbers, leftIndex, rightIndex, comparaisonsCounter) => {
-
-    if (!comparaisonsCounter) {
-     comparaisonsCounter = 1
-    } else {
-      comparaisonsCounter++;
-    }
-    if (!leftIndex) {
-      leftIndex = 0;
-    }
-    if (!rightIndex) {
-      rightIndex = numbers.length - 1;
-    }
-
-    if (leftIndex >= rightIndex) return
-    let index = partition(numbers, leftIndex, rightIndex)
-    // Recursion vers la gauche puis vers la droite
-    quickSort(numbers, leftIndex, index - 1, comparaisonsCounter);
-    quickSort(numbers, index + 1, rightIndex, comparaisonsCounter);
-    return `Tri rapide / Quicksort : ${comparaisonsCounter} ${count} comparaisons - [${numbers}]`;
-  }; */
